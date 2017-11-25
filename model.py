aa39b2b5156fb01800b723b0e74d27732a8bf00d
Gioia Iacopini <gio.iacopini@gmail.com>
2017-11-25 14:52:18 +0000

    progress
    create agents list and append elements in it

--- model.py
+++ model.py
@@ -7,86 +7,103 @@ This is a temporary script file.
 #import statements
 import random
 
+# lists 
+agents =[] #this is an empty list
+
 #Make a y and x variable (randomly generated)
 y0 = random.randint(0,100)
 x0 = random.randint(0,100)
 
-print (x0, y0)
+agents.append([x0,y0])
+
+print (agents)
 
 # Move x0 randomly
+# the (x0,y0) list is agents[0]. x0 is agents[0][0] and y0 is agents[0][1]
 
 if random.random() < 0.5:
-    x0 = x0 +1
+    agents[0][0]+= 1
 else:
-    x0 = x0 - 1
+    agents[0][0]-= 1
+
+print(agents[0][0])
+
 
 # Move y0 randomly
 
 if random.random() < 0.5: 
-    y0 = y0 + 1
+    agents[0][1]+= 1
 else: 
-    y0 = y0 -1 
-    
-print (x0, y0)
+    agents[0][1]-= 1
+
+print(agents[0][1])
+
     
 # Move x0 randomly
-
+    
 if random.random() < 0.5:
-    x0 = x0 +1
+    agents[0][0]+= 1
 else:
-    x0 = x0 - 1
+    agents[0][0]-= 1
+
+print(agents[0][0])
 
 # Move y0 randomly
 
 if random.random() < 0.5: 
-    y0 = y0 + 1
+    agents[0][1]+= 1
 else: 
-    y0 = y0 -1 
-    
-print (x0, y0)
+    agents[0][1]-= 1
+
+print(agents[0][1])
 
 # Create x1 and y1 (randomly generated)
 y1 = random.randint(0,100)
 x1 = random.randint(0,100)
 
-print (x1, y1)
+agents.append([x1,y1])
+
+print(agents)
 
 # Move x1 randomly
 
 if random.random() < 0.5:
-    x1 = x1 +1
+    agents[1][0]+= 1
 else:
-    x1 = x1 - 1
+    agents[1][0]-= 1
+
+print(agents[1][0])
 
 # Move y1 randomly
 
 if random.random() < 0.5: 
-    y1 = y1 + 1
+    agents[1][1]+= 1
 else: 
-    y1 = y1 -1 
+    agents[1][1]-= 1
     
-print (x1, y1)
-    
+print(agents[1][1])
+
 # Move x1 randomly
 
 if random.random() < 0.5:
-    x1 = x1 +1
+    agents[1][0]+= 1
 else:
-    x1 = x1 - 1
+    agents[1][0]-= 1
 
-# Move x1 randomly
-    
+print(agents[1][0])
+
+# Move y1 randomly
+
 if random.random() < 0.5: 
-    y1 = y1 + 1
+    agents[1][1]+= 1
 else: 
-    y1 = y1 -1 
+    agents[1][1]-= 1
     
-print (x1, y1)
-
+print(agents[1][1])
 
 # calculate the discance between the variables usign the Pythagorian theorem
 
-distance = ((x0-x1)**2 + (y0 - y1)**2)**0.5
+distance = ((agents[0][0]-agents[1][0])**2 + (agents[0][1] - agents[1][1])**2)**0.5
 print (distance)
 
 
