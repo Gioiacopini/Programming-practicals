9d1457b7757d3b40f7ea3711700d1ceab0bc92d3
Gioia Iacopini <gio.iacopini@gmail.com>
2017-11-25 15:38:30 +0000

    code shrinking
    added number of iterations and boundaries to avoid agents wandering off the plane.

--- model.py
+++ model.py
@@ -9,100 +9,43 @@ import random
 import operator
 import matplotlib.pyplot
 
+#This states how many agents we want to create
+num_of_agents = 10
+num_of_iterations = 100
+
 # lists 
 agents =[] #this is an empty list
 
-#Make a y and x variable (randomly generated) without creating separate variables.
+# randomly generate agents. This is a loop that creates 10 random coordinates. 
+
+for i in range(num_of_agents):
+    agents.append([random.randint(0,100),random.randint(0,100)])
+
 #this code appends random integers between 0 and 99 to the list [agents] created above. 
 agents.append([random.randint(0,99),random.randint(0,99)])
 
 print (agents)
 
-# Move x0 randomly
-# the (x0,y0) list is agents[0]. x0 is agents[0][0] and y0 is agents[0][1]
-
-if random.random() < 0.5:
-    agents[0][0]+= 1
-else:
-    agents[0][0]-= 1
-
-print(agents[0][0])
-
-
-# Move y0 randomly
-
-if random.random() < 0.5: 
-    agents[0][1]+= 1
-else: 
-    agents[0][1]-= 1
-
-print(agents[0][1])
-
+# this loop moves all 10 points 100 times. the (%99) at the end of the code makes the agents stay in the 100x100 plane (Tarus Method).
+for j in range(num_of_iterations):
+    for i in range(num_of_agents):
     
-# Move x0 randomly
+        if random.random() < 0.5: 
+            agents[i][0]+= 1 % 99
+        else: 
+            agents[i][0]-= 1 % 99
     
-if random.random() < 0.5:
-    agents[0][0]+= 1
-else:
-    agents[0][0]-= 1
-
-print(agents[0][0])
-
-# Move y0 randomly
-
-if random.random() < 0.5: 
-    agents[0][1]+= 1
-else: 
-    agents[0][1]-= 1
-
-print(agents[0][1])
-
-# Create x1 and y1 (randomly generated)
-
-agents.append([random.randint(0,99),random.randint(0,99)])
+        if random.random() < 0.5: 
+            agents[i][1]+= 1 % 99
+        else: 
+            agents[i][1]-= 1 % 99
 
 print(agents)
 
-# Move x1 randomly
-
-if random.random() < 0.5:
-    agents[1][0]+= 1
-else:
-    agents[1][0]-= 1
-
-print(agents[1][0])
-
-# Move y1 randomly
-
-if random.random() < 0.5: 
-    agents[1][1]+= 1
-else: 
-    agents[1][1]-= 1
-    
-print(agents[1][1])
-
-# Move x1 randomly
-
-if random.random() < 0.5:
-    agents[1][0]+= 1
-else:
-    agents[1][0]-= 1
-
-print(agents[1][0])
-
-# Move y1 randomly
-
-if random.random() < 0.5: 
-    agents[1][1]+= 1
-else: 
-    agents[1][1]-= 1
-    
-print(agents[1][1])
-
 # calculate the discance between the variables usign the Pythagorian theorem
 
-distance = ((agents[0][0]-agents[1][0])**2 + (agents[0][1] - agents[1][1])**2)**0.5
-print (distance)
+#distance = ((agents[0][0]-agents[1][0])**2 + (agents[0][1] - agents[1][1])**2)**0.5
+#print (distance)
 
 #let's run an analysis to work out which agent is furthest north (highest y)
 print(max(agents, key=operator.itemgetter(1)))
@@ -115,12 +58,11 @@ print(max(agents, key=operator.itemgetter(0)))
 
 matplotlib.pyplot.ylim(0, 99)
 matplotlib.pyplot.xlim(0, 99)
-matplotlib.pyplot.scatter(agents[0][1],agents[0][0])
-matplotlib.pyplot.scatter(agents[1][1],agents[1][0])
-# colour the agent furthest east a different color
-m = max(agents, key=operator.itemgetter(1))
-matplotlib.pyplot.scatter(m[1],m[0], color='red')
-
+for i in range(num_of_agents):
+    matplotlib.pyplot.scatter(agents[i][1],agents[i][0])
+    # colour the agent furthest east a different color
+    #m = max(agents, key=operator.itemgetter(1))
+    #matplotlib.pyplot.scatter(m[1],m[0], color='red')
 matplotlib.pyplot.show()
 
 
