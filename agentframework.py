b469d4ef5c42062743bbcec9cc9ee7b8a8d4df19
Gioia Iacopini <gio.iacopini@gmail.com>
2017-11-25 17:31:04 +0000

    file

--- agentframework.py
+++ agentframework.py
@@ -8,9 +8,12 @@ Created on Sat Nov 25 16:11:55 2017
 import random
 
 class Agent:
-    def __init__ (self):
+    def __init__ (self, environment, agents):
         self.x = random.randint(0,99)
         self.y = random.randint (0,99)
+        self.environment = environment
+        self.agents = agents
+        self.store = 0
         
 
     def move (self):
@@ -25,4 +28,28 @@ class Agent:
         else:
             self.y = (self.y - 1) % 100
         #print(self.x, self.y)
+    
+    def eat (self):
+        if self.environment[self.y][self.x]>10:
+            self.environment[self.y][self.x]-=10
+            self.store+=10
+        if 0<self.environment[self.y][self.x]<10:
+            self.store+=self.environment[self.y][self.x]
+            self.environment[self.y][self.x]-=self.environment[self.y][self.x]
+        if self.store > 1000:
+            self.environment[self.y][self.x]+=self.store
+            self.store = 0 
+            print(self.store)
         
+    def distance_between(self, agent):
+        return (((self.x-agent.x)**2) + ((self.y-agent.y)**2)**0.5)
+
+    def share_with_neighbours (self, neighborhood):
+        for agent in self.agents:
+            dist = self.distance_between(agent)
+            if dist <= neighborhood:
+                sum = self.store + agent.store
+                ave = sum/2
+                self.store = ave 
+                agent.store = ave
+                print("sharing "+str(dist) + " " + str(ave))
--- model.py
+++ model.py
@@ -5,38 +5,52 @@ Spyder Editor
 This is a temporary script file.
 """
 #import statements
-import random
-import operator
 import matplotlib.pyplot
-
 import agentframework
+import csv
+import random
 
-#definition declaration of functions
-
-def distance_between(agent0, agent1):
-    return (((agent0.x-agent0.x)**2) + ((agent1.y-agent1.y)**2)**0.5)
-
-#This states how many agents we want to create
 num_of_agents = 10
 num_of_iterations = 100
+neighborhood = 20
+store = 0
 
 # lists 
 agents =[] #this is an empty list
+environment=[]
 
 # randomly make agents
 
 for i in range(num_of_agents):
-    agents.append(agentframework.Agent())
+    agents.append(agentframework.Agent(environment, agents))
+    
+#read data 
 
-#move agents 
+file = open("in.txt", newline='')
+
+reader= csv.reader(file, quoting=csv.QUOTE.NONNUMERIC)
+for row in reader:
+#for each row in the file we create an empty row list
+    rowlist=[]
+#for each item in the rows we append the items to the rowlist
+    for item in row:
+        rowlist.append(item)
+#we append the rowlist into the environment
+    environment.append(rowlist)
+
+#make agents move, eat and interact with neighbours
 for j in range(num_of_iterations):
+#randomise the way agents are picked for the iterations
+    random.shuffle(agents)
     for i in range(num_of_agents):
         agents[i].move()
-    
+        agents[i].eat()
+        agents[i].share_with_neighbours(neighborhood)
 
-#plot the agents using matplotlib.pyplot
+#plot the agents and environment using matplotlib.pyplot
 matplotlib.pyplot.ylim(0, 99)
 matplotlib.pyplot.xlim(0, 99)
+matplotlib.pyplot.imshow(environment)
 for i in range(num_of_agents):
     matplotlib.pyplot.scatter(agents[i].x,agents[i].y)
     # colour the agent furthest east a different color
